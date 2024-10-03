deb9ff1654f5cd2891d384e150ec1b4a09c73b5f
oddwatcher@arc770 <science-35@outlook.com>
2024-10-03 15:59:52 +0800

    looking into details

--- backbone.py
+++ backbone.py
@@ -65,9 +65,12 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-epochs = 5
+epochs = 10
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dl, model, loss_fn, optimizer)
     test(test_dl, model, loss_fn)
 print("Done!")
+
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
